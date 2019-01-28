b051aa4f392bc4cd81a878fa606f2b09fa153b7b
zheng59521 <1421287137@qq.com>
2019-01-28 17:43:43 +0800

    完善用户查询

--- GraphQL/Query/UserQuery.php
+++ GraphQL/Query/UserQuery.php
@@ -19,19 +19,26 @@ class UserQuery extends Query
         return Type::listOf(GraphQL::type('user'));
     }
 
+
+    /*
+     * 数据库字段 => 请求参数
+     * */
     public function args() {
         return [
-            'id' => ['name' => 'id', 'type' => Type::int()],
-            'name' => ['name' => 'name', 'type' => Type::string()]
+            'id' => ['name' => '_id', 'type' => Type::int()],
+            'name' => ['name' => '_name', 'type' => Type::string()],
+            'j_id' => ['name' => '_jid', 'type' => Type::int()]
         ];
     }
 
     public function resolve($root, $args) {
         $user = new MyUser();
-        if (isset($args['id'])) { // 指定id时候
-            $user = $user::where('id' , $args['id']);
-        } else if(isset($args['name'])) { // 指定name时候
-            $user = $user::where('name', $args['name'])->limit(10);
+        if (isset($args['_id'])) { // 指定id时候
+            $user = $user::where('id' , $args['_id']);
+        } else if(isset($args['_name'])) { // 指定name时候
+            $user = $user::where('name', $args['_name'])->limit(10);
+        } else if(isset($args['_jid'])) { // 指定name时候
+            $user = $user::where('j_id', $args['_jid'])->limit(10);
         } else {
             $user = $user->limit(10);
         }
--- GraphQL/Type/UserType.php
+++ GraphQL/Type/UserType.php
@@ -7,6 +7,7 @@ use Graphql\Type\Definition\Type;
 use Folklore\GraphQL\Support\Type as GraphQLType;
 use App\Models\MyUser;
 use App\Models\Job;
+
 class UserType extends GraphQLType {
 
     /*
@@ -29,23 +30,68 @@ class UserType extends GraphQLType {
                 'type' => Type::nonNull(Type::int()),
                 'description' => '用户id',
             ],
-            'name' => [
+            'username' => [
                 'type' => Type::string(),
                 'description' => '用户名',
             ],
+            'jid' => [
+                'type' => Type::int(),
+                'description' => '关联id'
+            ],
             'job' => [
+                'args' => [
+                    'job_id' => [
+                        'type' => Type::int(),
+                        'description' => '工作id'
+                    ],
+                    'job_name' => [
+                        'type' => Type::string(),
+                        'description' => '工作名称'
+                    ]
+                ],
                 'type' => Type::listOf(GraphQL::type('job')),
                 'description' => '工作'
             ]
         ];
     }
 
+//    ------------------------------------------------
     /*
-     * If you want to resolve the field yourself, you can declare a method
-     * with the following format resolve[FIELD_NAME]Field()
-     * 对返回的字段进行处理
+     * resolve***Field方法
+     * 1.对返回字段进行关联数据库字段操作 (例如给数据库字段起个别名)
+     * 2.对返回的字段进行处理
      * */
-    protected function resolveNAMEField($root, $args) {
+
+    /*
+     * username => user.name
+     * */
+    protected function resolveUSERNAMEField($root, $args) {
         return (string)strtolower('用户名为 '.$root->name);
     }
+
+    /*
+     * jid => user.j_id
+     * */
+    protected function resolveJIDField($root, $args) {
+        return (int) $root->j_id;
+    }
+
+    /*
+     * job => jobTable
+     * */
+    public function resolveJOBField($root, $args) {
+        $job = $root->job();
+        if (isset($args['job_id'])) {
+            $job->where('id', $args['job_id']);
+        }
+        if (isset($args['job_name'])) {
+            $job->where('name', $args['job_name']);
+        }
+        return $job->get();
+    }
+
+//    --------------------------------------------------
+
+
+
 }
